3b97599b6f6de2111b099b6e8d43aeb9acf91907
ZCuak <alex_geancarlos@hotmail.com>
2024-08-23 17:33:58 -0500

    Proyecto Paises

---
+++ src/app/countries/services/countries.service.ts
@@ -0,0 +1,44 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { catchError, Observable, of } from 'rxjs';
+import { Country } from '../interfaces/country';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CountriesService {
+
+  private apiURl: string = 'https://restcountries.com/v3.1';
+  constructor(private http: HttpClient) { }
+
+  searchByCapital( term: string ): Observable<Country[]>{
+    const url = `${ this.apiURl }/capital/${ term }`
+    return this.http.get<Country[]>( url )
+      .pipe(
+        catchError( error => of([]) )
+      );
+  }
+  searchByCountry( term: string ): Observable<Country[]>{
+    const url = `${ this.apiURl }/name/${ term }`
+    return this.http.get<Country[]>( url )
+      .pipe(
+        catchError( error => of([]) )
+      );
+  }
+  searchByRegion( term: string ): Observable<Country[]>{
+    const url = `${ this.apiURl }/region/${ term }`
+    return this.http.get<Country[]>( url )
+      .pipe(
+        catchError( error => of([]) )
+      );
+  }
+
+
+  searchCountryByAlphaCode( code: string ): Observable<Country[]>{
+    const url = `${ this.apiURl }/alpha/${ code }`
+    return this.http.get<Country[]>( url )
+      .pipe(
+        catchError( error => of([]) ) 
+      );
+  }
+}
