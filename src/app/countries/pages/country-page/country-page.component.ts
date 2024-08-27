d680927dca9b7436d212d0ff05058f7e2d87d979
ZCuak <alex_geancarlos@hotmail.com>
2024-08-27 17:04:50 -0500

    Fin

--- src/app/countries/pages/country-page/country-page.component.ts
+++ src/app/countries/pages/country-page/country-page.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { CountriesService } from '../../services/countries.service';
+import { switchMap } from 'rxjs';
+import { Country } from '../../interfaces/country';
 
 @Component({
   selector: 'app-country-page',
@@ -9,17 +11,29 @@ import { CountriesService } from '../../services/countries.service';
 })
 export class CountryPageComponent implements OnInit{
 
-  constructor( private activatedRoute: ActivatedRoute, private countriesServices: CountriesService ){}
+  public country?: Country;
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    private countriesServices: CountriesService,
+    private router: Router,
+  ){}
 
   ngOnInit(): void {
     this.activatedRoute.params
-    .subscribe( ({ id }) => {
-      this.countriesServices.searchCountryByAlphaCode(id)
-      .subscribe( country => {
-        
-      });
+    .pipe(
+      switchMap(({ id }) => this.countriesServices.searchCountryByAlphaCode(id))
+    )
+    .subscribe( country => {
+
+      if(!country){
+        return this.router.navigateByUrl('');
+      }
+      return this.country = country
+
     });
   }
+
+
 
 
 
